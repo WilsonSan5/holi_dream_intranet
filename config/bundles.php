577f73ae0a25364782f3893221b0f5c318fc82eb
WilsonSan5 <wilson.san57@gmail.com>
2024-03-13 17:41:11 +0100

    added turbo symfony ux

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => false],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -14,8 +14,6 @@ class LoginController extends AbstractController
 
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
